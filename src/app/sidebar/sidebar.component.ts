44fdd3fff7a6e10c274646b64b7965bbddf33617
ganesh-91 <ganesh.khaire91@gmail.com>
2017-09-18 12:28:05 +0530

    added header and sidebar components

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SidebarComponent',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  title = 'app';
+}
