cf5be34a80ebbee0994d3327053b95123c905f9b
ganesh-91 <ganesh.khaire91@gmail.com>
2017-09-18 14:54:44 +0530

    added login page + added login to module import + added login route + added dashboard route + moved all dashboard component to dashboard folder + added content view wrapper and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { ContentViewWrapperComponent } from './contentViewWrapper/contentViewWrapper.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+import { ContentViewWrapperComponent } from './dashboard/contentViewWrapper/contentViewWrapper.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Login } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    ContentViewWrapperComponent
+    ContentViewWrapperComponent,
+    DashboardComponent,
+    Login
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class Login {
+  title = 'app';
+}
